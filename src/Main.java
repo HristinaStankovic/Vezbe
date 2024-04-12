3727f79fb74460845e990adc0b0a8e24a4cc9833
Hristina Stankovic <hristina.stankovic@nfinnova.com>
2024-04-12 17:13:32 +0200

    More changes in Main class

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,15 @@
 
                 Nizovi2 nizovi2 = new Nizovi2();
                 nizovi2.generateRandomCombinations(nizovi2.words, nizovi2.numberOfCombinations);
+
+                System.out.println("---------------------------------------------------------------------------------------------");
+
+                Kalkulator kalkulator = new Kalkulator();
+
+                System.out.println(kalkulator.sub());
+                System.out.println( kalkulator.add());
+                System.out.println(kalkulator.div());
+                System.out.println(kalkulator.mul());
             }
+
             }
