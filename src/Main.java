1e97ebfb1afd9f28ee24a22c8a996142f0c3087e
Hristina Stankovic <hristina.stankovic@nfinnova.com>
2024-04-12 17:10:41 +0200

    Changes in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-import java.util.Random;
 
         public class Main {
             public static void main(String[] args) {
+                Nizovi1 nizovi = new Nizovi1();
+                nizovi.generateSentences(nizovi.random_stvari, nizovi.modeli, nizovi.cene, nizovi.numberOfCombinations);
 
-        }
+                System.out.println("---------------------------------------------------------------------------------------------");
 
-        }
+                Nizovi2 nizovi2 = new Nizovi2();
+                nizovi2.generateRandomCombinations(nizovi2.words, nizovi2.numberOfCombinations);
+            }
+            }
